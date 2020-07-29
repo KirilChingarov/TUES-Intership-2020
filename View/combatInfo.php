978d738b0f4c54d3521c6990d4a8ec9c5f269171
Kiril Chingarov <kiril.chingarov@gmail.com>
2020-07-29 16:40:10 +0300

    Simple UI (#8)
    * Added takeDamage and death effect for enemy
    
    * Fixed bug with death animation
    
    * Added playerHit function
    
    * Added player character death animation
    
    * Disabling of buttons during combat phase

--- View/combatInfo.php
+++ View/combatInfo.php
@@ -19,6 +19,7 @@
         <title>Combat Info</title>
         <link rel="stylesheet" href="View/css/combatInfo.css" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script>
             var targetId;
             var currentTurn = <?= $currentTurn?>;
@@ -26,11 +27,16 @@
             var attackerId = combatInfo.turns[currentTurn][1];
             var turns = combatInfo.turns;
             var enemyTargetId;
+            var battleLost = false;
             const LAST_PLAYER_TURN = 6;
             const LAST_ENEMY_TURN = 7;
             const NEW_ROUND = 0;
+            const PLAYER_TURN = 0;
+            const ENEMY_TURN = 1;
 
             function attackEnemy(){
+                disableButtons();
+
                 var data = new FormData();
                 data.append('targetId', targetId);
                 data.append('attackerId', attackerId);
@@ -39,7 +45,8 @@
                 var xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function(){
                     if(this.readyState == 4 && this.status == 200){
-                        if(parseResponse(this.responseText)){
+                        takeDamage(targetId);
+                        if(parseResponse(this.responseText, PLAYER_TURN)){
                             enemyAttack();
                         }
                     }
@@ -72,7 +79,7 @@
                 var xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function(){
                     if(this.readyState == 4 && this.status == 200){
-                        parseResponse(this.responseText);
+                        parseResponse(this.responseText, ENEMY_TURN);
                         checkNextTurn();
                     }
                 }
@@ -95,17 +102,27 @@
 
                     enemyAttack();
                 }
+                else{
+                    enableButtons();
+                }
             }
 
-            function parseResponse(responseText){
+            function parseResponse(responseText, turn){
                 if(!checkEndBattle(responseText)){
                     combatInfo = jQuery.parseJSON(responseText);
                     turns = combatInfo.turns;
                     currentTurn = combatInfo.currentTurn;
-                    attackerId = combatInfo.turns[currentTurn][1];
 
-                    updatePlayerPartyMembers(combatInfo.playerParty.members);
-                    updateEnemyPartyMembers(combatInfo.enemyParty.members);
+                    if(turn == PLAYER_TURN){
+                        updateEnemyPartyMembers(combatInfo.enemyParty.members);
+                    }
+                    if(turn == ENEMY_TURN){
+                        hitPlayer(enemyTargetId, attackerId);
+                        updatePlayerPartyMembers(combatInfo.playerParty.members);
+                    }
+                    
+                    attackerId = combatInfo.turns[currentTurn][1];
+                    updatePartyTurns(combatInfo.playerParty.members, combatInfo.enemyParty.members);
 
                     return true;
                 }
@@ -119,7 +136,11 @@
                     jQuery.parseJSON(responseText);
                     return false;
                 } catch (error) {
-                    document.write(responseText);
+                    if(!battleLost){
+                        console.log(responseText);
+                        document.write(responseText);
+                        battleLost = true;
+                    }
                     return true;
                 }
             }
@@ -128,19 +149,11 @@
 
                 for(var i = 0;i < 4;i++){
                     var playerStats = "#player-" + i;
-                    if(playerPartyMembers[i].characterIsDead){
+                    if(playerPartyMembers[i].characterIsDead && !$(playerStats).parent().is(":hidden")){
                         $(playerStats).parent().attr('class', 'character-dead');
+                        $(playerStats).parent().effect("explode", {pieces: 49}, 400, callback(playerStats));
                     }
                     else{
-                        if(turns[currentTurn][0] == 'p' && turns[currentTurn][1] == i){
-                            $(playerStats).parent().attr('class', 'character-turn');
-                        }
-                        else if(currentTurn < LAST_PLAYER_TURN && turns[currentTurn + 1][0] == 'p' && turns[currentTurn + 1][1] == i){
-                            $(playerStats).parent().attr('class', 'character-next-turn');
-                        }
-                        else{
-                            $(playerStats).parent().attr('class', 'character');
-                        }
                         $(playerStats + ">.health").text("Health: " + playerPartyMembers[i].characterHealth);
                     }
                 }
@@ -150,21 +163,68 @@
                 
                 for(var i = 0;i < 4;i++){
                     var enemyStats = "#enemy-" + i;
-                    if(enemyPartyMembers[i].characterIsDead){
-                        $(enemyStats).parent().attr('class', 'character-dead');
+                    if(enemyPartyMembers[i].characterIsDead && !$(enemyStats).parent().is(":hidden")){
+                        $(enemyStats).parent().effect("explode", {pieces: 49}, 400, callback(enemyStats));
                     }
                     else{
-                        if(turns[currentTurn][0] == 'e' && turns[currentTurn][1] == i){
-                            $(enemyStats).parent().attr('class', 'enemy character-turn');
-                        }
-                        else if(currentTurn < LAST_ENEMY_TURN && turns[currentTurn + 1][0] == 'e' && turns[currentTurn + 1][1] == i){
-                            $(enemyStats).parent().attr('class', 'enemy character-next-turn');
-                        }
-                        else{
-                            $(enemyStats).parent().attr('class', 'enemy character');
-                        }
                         $(enemyStats + ">.health").text("Health: " + enemyPartyMembers[i].characterHealth);
                     }
+                }
+            }
+
+            function updatePartyTurns(playerPartyMembers, enemyPartyMembers){
+                for(var i = 0;i < 4;i++){
+                    // player turns ---------------------------
+                    var playerStats = "#player-" + i;
+                    if(turns[currentTurn][0] == 'p' && turns[currentTurn][1] == i){
+                        $(playerStats).parent().attr('class', 'character-turn');
+                    }
+                    else if(currentTurn < LAST_PLAYER_TURN && turns[currentTurn + 1][0] == 'p' && turns[currentTurn + 1][1] == i){
+                        $(playerStats).parent().attr('class', 'character-next-turn');
+                    }
+                    else{
+                        $(playerStats).parent().attr('class', 'character');
+                    }
+
+                    // enemy turns ---------------------------
+                    var enemyStats = "#enemy-" + i;
+                    if(turns[currentTurn][0] == 'e' && turns[currentTurn][1] == i){
+                        $(enemyStats).parent().attr('class', 'character-turn');
+                    }
+                    else if(currentTurn < LAST_ENEMY_TURN && turns[currentTurn + 1][0] == 'e' && turns[currentTurn + 1][1] == i){
+                        $(enemyStats).parent().attr('class', 'character-next-turn');
+                    }
+                    else{
+                        $(enemyStats).parent().attr('class', 'character');
+                    }
+                }
+            }
+
+            function takeDamage(buttonId){
+                $("#" + buttonId).parent().effect("pulsate");
+            }
+
+            function hitPlayer(targetId, attackerId){
+                if(combatInfo.enemyParty.members[attackerId] != null && !combatInfo.enemyParty.members[attackerId].characterIsDead){
+                    $("#player-" + targetId).parent().effect("pulsate");
+                }
+            }
+
+            function callback(target){
+                setTimeout(function(){
+                    $(target).parent().removeAttr("style").hide();
+                }, 400);
+            }
+
+            function disableButtons(){
+                for(var i = 0;i < 4;i++){
+                    $("#" + i).attr('disabled', true);
+                }
+            }
+
+            function enableButtons(){
+                for(var i = 0;i < 4;i++){
+                    $("#" + i).attr('disabled', false);
                 }
             }
         </script>
@@ -222,10 +282,10 @@
                         foreach($enemyPartyMembers as $epm){
                             $cssClass;
                             if($turns[$currentTurn+1][1] === $epmNum){
-                                $cssClass = 'enemy character-next-turn';
+                                $cssClass = 'character-next-turn';
                             }
                             else{
-                                $cssClass = 'character enemy';
+                                $cssClass = 'character';
                             }
                             if($epm->isCharacterDead()){
                                 $cssClass = 'character-dead';
